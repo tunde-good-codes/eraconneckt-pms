3d424326390186c57f75fdad4c85bd80e0b6ced2
tundenewcodes <fasasipelumi1652@gmail.com>
2025-11-13 22:04:12 +0100

    semantics

--- src/assets/data/feature-card-data/featureCardData.ts
+++ src/assets/data/feature-card-data/featureCardData.ts
@@ -20,7 +20,7 @@ export const featureCardData: IFeatureCard = [
 		id: 1,
 		title: "Project & Task Management",
 		description:
-			"Stay organized, meet deadlines, and boost team productivity with our powerful task management system. Assign tasks, track progress, and collaborate seamlessly—all in one place!",
+			"Stay organized, meet deadlines, and boost team productivity with our powerful task management system. Assign tasks, track progress, and collaborate seamlessly — all in one place!",
 		icon: "https://res.cloudinary.com/bacf/image/upload/v1735049589/HRTechish/icon-park-solid_success_fjnfsw.svg",
 		featureItems: [
 			"Task Assignment & Tracking: Assign tasks, set deadlines, and track progress.",
